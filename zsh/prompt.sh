66fc2fb915b22ebe15201b93f269619f98d56072
Marc André Freiheit <marcandre@freiheit.software>
2018-09-22 12:05:00 +0200

    Add git remote information to terminal

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -4,6 +4,53 @@ autoload -U colors && colors
 
 setopt PROMPT_SUBST
 
+export __GIT_PROMPT_DIR=/Users/marcfreiheit/github/dotfiles/zsh/git-prompt
+export GIT_PROMPT_EXECUTABLE=${GIT_PROMPT_EXECUTABLE:-"python"}
+
+add-zsh-hook chpwd chpwd_update_git_vars
+add-zsh-hook preexec preexec_update_git_vars
+add-zsh-hook precmd precmd_update_git_vars
+
+## Function definitions
+function preexec_update_git_vars() {
+    case "$2" in
+        git*|hub*|gh*|stg*)
+        __EXECUTED_GIT_COMMAND=1
+        ;;
+    esac
+}
+
+function precmd_update_git_vars() {
+    if [ -n "$__EXECUTED_GIT_COMMAND" ] || [ ! -n "$ZSH_THEME_GIT_PROMPT_CACHE" ]; then
+        update_current_git_vars
+        unset __EXECUTED_GIT_COMMAND
+    fi
+}
+
+function chpwd_update_git_vars() {
+    update_current_git_vars
+}
+
+function update_current_git_vars() {
+    unset __CURRENT_GIT_STATUS
+
+    if [[ "$GIT_PROMPT_EXECUTABLE" == "python" ]]; then
+        local gitstatus="$__GIT_PROMPT_DIR/gitstatus.py"
+        _GIT_STATUS=`python ${gitstatus} 2>/dev/null`
+    fi
+    if [[ "$GIT_PROMPT_EXECUTABLE" == "haskell" ]]; then
+        _GIT_STATUS=`git status --porcelain --branch &> /dev/null | $__GIT_PROMPT_DIR/src/.bin/gitstatus`
+    fi
+     __CURRENT_GIT_STATUS=("${(@s: :)_GIT_STATUS}")
+	GIT_BRANCH=$__CURRENT_GIT_STATUS[1]
+	GIT_AHEAD=$__CURRENT_GIT_STATUS[2]
+	GIT_BEHIND=$__CURRENT_GIT_STATUS[3]
+	GIT_STAGED=$__CURRENT_GIT_STATUS[4]
+	GIT_CONFLICTS=$__CURRENT_GIT_STATUS[5]
+	GIT_CHANGED=$__CURRENT_GIT_STATUS[6]
+	GIT_UNTRACKED=$__CURRENT_GIT_STATUS[7]
+}
+
 set_prompt() {
 
 	# [
@@ -22,6 +69,12 @@ set_prompt() {
 		if [ $(git status --short | wc -l) -gt 0 ]; then 
 			PS1+="%{$fg[yellow]%}+$(git status --short | wc -l | awk '{$1=$1};1')%{$reset_color%}"
 		fi
+	  if [ "$GIT_BEHIND" -ne "0" ]; then
+		  PS1+="$STATUS$ZSH_THEME_GIT_PROMPT_BEHIND$GIT_BEHIND%{${reset_color}%}"
+	  fi
+	  if [ "$GIT_AHEAD" -ne "0" ]; then
+		  PS1+="$STATUS$ZSH_THEME_GIT_PROMPT_AHEAD$GIT_AHEAD%{${reset_color}%}"
+	  fi
 	fi
 
 
@@ -44,3 +97,6 @@ set_prompt() {
 
 precmd_functions+=set_prompt
 
+# Default values for the appearance of the prompt. Configure at will.
+ZSH_THEME_GIT_PROMPT_BEHIND="%{$fg[green]%}%{↓%G%}"
+ZSH_THEME_GIT_PROMPT_AHEAD="%{$fg[green]%}%{↑%G%}"
